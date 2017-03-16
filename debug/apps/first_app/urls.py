02c2c2273220b7f96586b931a11e523b03b194ac
phil-krull <pkrull@codingdojo.com>
2017-03-16 08:56:01 -0700

    updated challenge

--- debug/apps/first_app/urls.py
+++ debug/apps/first_app/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^', views.index),
-    url(r'^show/(?P<name>.*$)', views.show),
+    url(r'^', views.index, name = 'index'),
+    url(r'^show/(?P<name>.*$)', views.show, name = 'show'),
 ]
--- debug/debug/urls.py
+++ debug/debug/urls.py
@@ -13,8 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^', include('apps.first_app.urls')),
+    url(r'^', include('apps.first_app.urls', namespace = 'home')),
 ]
