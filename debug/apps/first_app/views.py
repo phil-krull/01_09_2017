063fe3544389d9579274f5691f34cad548a87197
Phil Krull <pkrull@codingdojo.com>
2019-09-11 14:03:40 -0500

    updated with new errors

--- debug/apps/first_app/views.py
+++ debug/apps/first_app/views.py
@@ -4,12 +4,16 @@ from django.shortcuts import render
 
 def index(request):
     if 'count' not in request.session:
-        request.session.count = 0
+        request.session['count'] = 0
     return render(request, 'first_app/index.html')
 
 def show(request):
-    request.session.count += 1
+    request.session['count'] += 1
     context = {
         'name': name,
     }
     return render(request, 'first_app/show.html')
+
+def start_over(request):
+    request.session.clear()
+    return redirect('/')
