4b8d5b73707efd4b65eb2fee7441f50c5397d9b8
Phil Krull <pkrull@codingdojo.com>
2019-09-11 14:22:55 -0500

    making final changes

--- debug/apps/first_app/urls.py
+++ debug/apps/first_app/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^', views.index),
     url(r'^show/(?P<name>.*$)', views.show),
-    url(r'^start_over', views.start_over)
+    url(r'^start_over$', views.start_over)
 ]
