65c5f5a018ce1381c1784c79805ae014f2c85806
Phil Krull <pkrull@codingdojo.com>
2019-09-11 13:44:15 -0500

    updated git ignore

--- debug/apps/first_app/views.py
+++ debug/apps/first_app/views.py
@@ -3,9 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
+    if 'count' not in request.session:
+        request.session.count = 0
     return render(request, 'first_app/index.html')
 
 def show(request):
+    request.session.count += 1
     context = {
         'name': name,
     }
